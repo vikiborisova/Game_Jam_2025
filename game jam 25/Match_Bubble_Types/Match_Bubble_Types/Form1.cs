a833a2d85b7fa3eeb30fa5b172bdb5cb78a3f9f9
Viki <67110330+vikiborisova@users.noreply.github.com>
2025-01-23 21:35:46 +0200

    Match Bubble Types Game
    new photos need to be added to the "pics" folder

---
+++ game jam 25/Match_Bubble_Types/Match_Bubble_Types/Form1.cs
@@ -0,0 +1,153 @@
+namespace Match_Bubble_Types
+{
+    public partial class Match_Bubble_Types : Form
+    {
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        string firstChoice;
+        string secondChoice;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totalTime = 60;
+        int countDownTime;
+        bool gameOver = false;
+        public Match_Bubble_Types()
+        {
+            InitializeComponent();
+            LoadPictures();
+        }
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            countDownTime--;
+            lblTimeLeft.Text = "Time Left: " + countDownTime;
+            if (countDownTime < 1)
+            {
+                GameOver("Times Up, You Lose");
+                foreach (PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("pics/" + (string)x.Tag + ".png");
+                    }
+                }
+            }
+        }
+
+        private void LoadPictures()
+        {
+            int leftPos = 40;
+            int topPos = 40;
+            int rows = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 100;
+                newPic.Width = 100;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+                if (rows < 3)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 120;
+                }
+                if (rows == 3)
+                {
+                    leftPos = 40;
+                    topPos += 120;
+                    rows = 0;
+                }
+            }
+            RestartGame();
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                return;
+            }
+            if (firstChoice == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    picA.Image = Image.FromFile("pics/" + (string)picA.Tag + ".png");
+                    firstChoice = (string)picA.Tag;
+                }
+            }
+            else if (secondChoice == null)
+            {
+                picB = sender as PictureBox;
+                if (picB.Tag != null && picB.Image == null)
+                {
+                    picB.Image = Image.FromFile("pics/" + (string)picB.Tag + ".png");
+                    secondChoice = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+        }
+
+
+
+        private void RestartGame()
+        {
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+
+            lblTimeLeft.Text = "Time Left: " + totalTime;
+            gameOver = false;
+            GameTimer.Start();
+            countDownTime = totalTime;
+
+        }
+
+
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+            if (firstChoice == secondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            firstChoice = null;
+            secondChoice = null;
+            foreach (PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                GameOver("Great Work, You Win!!!!");
+            }
+
+        }
+        private void GameOver(string msg)
+        {
+            GameTimer.Stop();
+            gameOver = true;
+        }
+    }
+}
