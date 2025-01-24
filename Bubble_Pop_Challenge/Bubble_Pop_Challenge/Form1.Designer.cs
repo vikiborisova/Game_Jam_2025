35971125bd328807cacfdf508012d570456bb31b
Viki <67110330+vikiborisova@users.noreply.github.com>
2025-01-24 11:08:46 +0200

    Bubble Pop Challenge Game Upload

---
+++ Bubble_Pop_Challenge/Bubble_Pop_Challenge/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Bubble_Pop_Challenge
+{
+    partial class Bubble_Pop_Challenge
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(43, 81);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(61, 20);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Bubble_Pop_Challenge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.lblScore);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Bubble_Pop_Challenge";
+            this.Text = "Bubble Pop Challenge";
+            this.Load += new System.EventHandler(this.Bubble_Pop_Challenge_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
