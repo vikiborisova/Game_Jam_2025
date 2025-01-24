3b4b99090a525169971613604f3a1c94512aa28d
natti-d <98655592+natti-d@users.noreply.github.com>
2025-01-24 05:40:48 +0200

    Add files via upload

---
+++ bubblesSay/bubblesSay/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace bubblesSay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.basic6 = new System.Windows.Forms.PictureBox();
+            this.basic5 = new System.Windows.Forms.PictureBox();
+            this.basic4 = new System.Windows.Forms.PictureBox();
+            this.basic3 = new System.Windows.Forms.PictureBox();
+            this.basic2 = new System.Windows.Forms.PictureBox();
+            this.basic1 = new System.Windows.Forms.PictureBox();
+            this.lvlLbl = new System.Windows.Forms.Label();
+            this.PurpleBbl_Says = new System.Windows.Forms.PictureBox();
+            this.BlueBbl_Says = new System.Windows.Forms.PictureBox();
+            this.AquaBbl_Says = new System.Windows.Forms.PictureBox();
+            this.GreenBbl_Says = new System.Windows.Forms.PictureBox();
+            this.YellowBbl_Says = new System.Windows.Forms.PictureBox();
+            this.RedBbl_Says = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.basic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PurpleBbl_Says)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBbl_Says)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AquaBbl_Says)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBbl_Says)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowBbl_Says)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBbl_Says)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // basic6
+            // 
+            this.basic6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic6.Image = ((System.Drawing.Image)(resources.GetObject("basic6.Image")));
+            this.basic6.Location = new System.Drawing.Point(1022, 451);
+            this.basic6.Name = "basic6";
+            this.basic6.Size = new System.Drawing.Size(222, 222);
+            this.basic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic6.TabIndex = 25;
+            this.basic6.TabStop = false;
+            this.basic6.Click += new System.EventHandler(this.basic6_Click);
+            // 
+            // basic5
+            // 
+            this.basic5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic5.Image = ((System.Drawing.Image)(resources.GetObject("basic5.Image")));
+            this.basic5.Location = new System.Drawing.Point(561, 451);
+            this.basic5.Name = "basic5";
+            this.basic5.Size = new System.Drawing.Size(222, 222);
+            this.basic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic5.TabIndex = 24;
+            this.basic5.TabStop = false;
+            this.basic5.Click += new System.EventHandler(this.basic5_Click);
+            // 
+            // basic4
+            // 
+            this.basic4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic4.Image = ((System.Drawing.Image)(resources.GetObject("basic4.Image")));
+            this.basic4.Location = new System.Drawing.Point(105, 451);
+            this.basic4.Name = "basic4";
+            this.basic4.Size = new System.Drawing.Size(222, 222);
+            this.basic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic4.TabIndex = 23;
+            this.basic4.TabStop = false;
+            this.basic4.Click += new System.EventHandler(this.basic4_Click);
+            // 
+            // basic3
+            // 
+            this.basic3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic3.Image = ((System.Drawing.Image)(resources.GetObject("basic3.Image")));
+            this.basic3.Location = new System.Drawing.Point(1022, 48);
+            this.basic3.Name = "basic3";
+            this.basic3.Size = new System.Drawing.Size(222, 222);
+            this.basic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic3.TabIndex = 22;
+            this.basic3.TabStop = false;
+            this.basic3.Click += new System.EventHandler(this.basic3_Click);
+            // 
+            // basic2
+            // 
+            this.basic2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic2.Image = ((System.Drawing.Image)(resources.GetObject("basic2.Image")));
+            this.basic2.Location = new System.Drawing.Point(561, 48);
+            this.basic2.Name = "basic2";
+            this.basic2.Size = new System.Drawing.Size(222, 222);
+            this.basic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic2.TabIndex = 21;
+            this.basic2.TabStop = false;
+            this.basic2.Click += new System.EventHandler(this.basic2_Click);
+            // 
+            // basic1
+            // 
+            this.basic1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.basic1.Image = ((System.Drawing.Image)(resources.GetObject("basic1.Image")));
+            this.basic1.Location = new System.Drawing.Point(105, 48);
+            this.basic1.Name = "basic1";
+            this.basic1.Size = new System.Drawing.Size(222, 222);
+            this.basic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.basic1.TabIndex = 20;
+            this.basic1.TabStop = false;
+            this.basic1.Click += new System.EventHandler(this.basic1_Click);
+            // 
+            // lvlLbl
+            // 
+            this.lvlLbl.AutoSize = true;
+            this.lvlLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlLbl.Location = new System.Drawing.Point(625, 350);
+            this.lvlLbl.Name = "lvlLbl";
+            this.lvlLbl.Size = new System.Drawing.Size(100, 29);
+            this.lvlLbl.TabIndex = 19;
+            this.lvlLbl.Text = "Ниво: 1";
+            // 
+            // PurpleBbl_Says
+            // 
+            this.PurpleBbl_Says.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(95)))), ((int)(((byte)(211)))));
+            this.PurpleBbl_Says.Image = global::bubblesSay.Properties.Resources.PurpleBbl_Says;
+            this.PurpleBbl_Says.Location = new System.Drawing.Point(1022, 451);
+            this.PurpleBbl_Says.Name = "PurpleBbl_Says";
+            this.PurpleBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.PurpleBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PurpleBbl_Says.TabIndex = 18;
+            this.PurpleBbl_Says.TabStop = false;
+            // 
+            // BlueBbl_Says
+            // 
+            this.BlueBbl_Says.BackColor = System.Drawing.Color.Blue;
+            this.BlueBbl_Says.Image = global::bubblesSay.Properties.Resources.BlueBbl_Says;
+            this.BlueBbl_Says.Location = new System.Drawing.Point(561, 451);
+            this.BlueBbl_Says.Name = "BlueBbl_Says";
+            this.BlueBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.BlueBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BlueBbl_Says.TabIndex = 17;
+            this.BlueBbl_Says.TabStop = false;
+            // 
+            // AquaBbl_Says
+            // 
+            this.AquaBbl_Says.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(212)))));
+            this.AquaBbl_Says.Image = global::bubblesSay.Properties.Resources.AquaBbl_Says;
+            this.AquaBbl_Says.Location = new System.Drawing.Point(105, 451);
+            this.AquaBbl_Says.Name = "AquaBbl_Says";
+            this.AquaBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.AquaBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AquaBbl_Says.TabIndex = 16;
+            this.AquaBbl_Says.TabStop = false;
+            // 
+            // GreenBbl_Says
+            // 
+            this.GreenBbl_Says.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(160)))), ((int)(((byte)(44)))));
+            this.GreenBbl_Says.Image = global::bubblesSay.Properties.Resources.GreenBbl_Says;
+            this.GreenBbl_Says.Location = new System.Drawing.Point(1022, 48);
+            this.GreenBbl_Says.Name = "GreenBbl_Says";
+            this.GreenBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.GreenBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GreenBbl_Says.TabIndex = 15;
+            this.GreenBbl_Says.TabStop = false;
+            // 
+            // YellowBbl_Says
+            // 
+            this.YellowBbl_Says.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(212)))), ((int)(((byte)(42)))));
+            this.YellowBbl_Says.Image = global::bubblesSay.Properties.Resources.YellowBbl_Says;
+            this.YellowBbl_Says.Location = new System.Drawing.Point(561, 48);
+            this.YellowBbl_Says.Name = "YellowBbl_Says";
+            this.YellowBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.YellowBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.YellowBbl_Says.TabIndex = 14;
+            this.YellowBbl_Says.TabStop = false;
+            // 
+            // RedBbl_Says
+            // 
+            this.RedBbl_Says.BackColor = System.Drawing.Color.Red;
+            this.RedBbl_Says.Image = global::bubblesSay.Properties.Resources.RedBbl_Says;
+            this.RedBbl_Says.Location = new System.Drawing.Point(105, 48);
+            this.RedBbl_Says.Name = "RedBbl_Says";
+            this.RedBbl_Says.Size = new System.Drawing.Size(222, 222);
+            this.RedBbl_Says.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RedBbl_Says.TabIndex = 13;
+            this.RedBbl_Says.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.basic6);
+            this.Controls.Add(this.basic5);
+            this.Controls.Add(this.basic4);
+            this.Controls.Add(this.basic3);
+            this.Controls.Add(this.basic2);
+            this.Controls.Add(this.basic1);
+            this.Controls.Add(this.lvlLbl);
+            this.Controls.Add(this.PurpleBbl_Says);
+            this.Controls.Add(this.BlueBbl_Says);
+            this.Controls.Add(this.AquaBbl_Says);
+            this.Controls.Add(this.GreenBbl_Says);
+            this.Controls.Add(this.YellowBbl_Says);
+            this.Controls.Add(this.RedBbl_Says);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.basic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PurpleBbl_Says)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBbl_Says)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AquaBbl_Says)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBbl_Says)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowBbl_Says)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBbl_Says)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox basic6;
+        private System.Windows.Forms.PictureBox basic5;
+        private System.Windows.Forms.PictureBox basic4;
+        private System.Windows.Forms.PictureBox basic3;
+        private System.Windows.Forms.PictureBox basic2;
+        private System.Windows.Forms.PictureBox basic1;
+        private System.Windows.Forms.Label lvlLbl;
+        private System.Windows.Forms.PictureBox PurpleBbl_Says;
+        private System.Windows.Forms.PictureBox BlueBbl_Says;
+        private System.Windows.Forms.PictureBox AquaBbl_Says;
+        private System.Windows.Forms.PictureBox GreenBbl_Says;
+        private System.Windows.Forms.PictureBox YellowBbl_Says;
+        private System.Windows.Forms.PictureBox RedBbl_Says;
+    }
+}
+
---
+++ bubblesSay/bubblesSay/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bubblesSay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        1 - червено
+        2 - жълто
+        3 - зелено
+        4 - аква
+        5 - синьо
+        6 - лилаво
+         */
+
+        List<int> bubbleSort = new List<int>();
+        int level = -1;
+        int clickCount = -1;
+        List<Control> controlList;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Играта започва!");
+            controlList = new List<Control> { basic1, basic2, basic3, basic4, basic5, basic6 };
+            startLevel();
+        }
+
+        private async void startLevel()
+        {
+            level++;
+            if(level == 10)
+            {
+                MessageBox.Show("Спечелихте играта!");
+                return;
+            }
+            lvlLbl.Text = "Ниво: " + (level+1).ToString();
+            clickCount = -1;
+
+            await Task.Delay(1000);
+            Random randomBbl = new Random();
+            int bubbleNum = randomBbl.Next(1, 7);
+            bubbleSort.Add(bubbleNum);
+
+            foreach (int i in bubbleSort)
+            {
+                controlList[i - 1].Visible = false;
+                controlList[i-1].Visible = false;
+                await Task.Delay(1000);
+
+                controlList[i-1].Visible = true;
+                await Task.Delay(500);
+            }
+        }
+
+        private void newGame()
+        {
+            level = -1;
+            clickCount = -1;
+            bubbleSort.Clear();
+            foreach(Control control in controlList)
+            {
+                control.Visible = true;
+            }
+            startLevel();
+
+        }
+
+        private async void basic1_Click(object sender, EventArgs e)
+        {
+            controlList[0].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[0].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 1)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+
+        private async void basic2_Click(object sender, EventArgs e)
+        {
+            controlList[1].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[1].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 2)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+
+        private async void basic3_Click(object sender, EventArgs e)
+        {
+            controlList[2].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[2].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 3)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+
+        private async void basic4_Click(object sender, EventArgs e)
+        {
+            controlList[3].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[3].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 4)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+
+        private async void basic5_Click(object sender, EventArgs e)
+        {
+            controlList[4].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[4].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 5)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+
+        private async void basic6_Click(object sender, EventArgs e)
+        {
+            controlList[5].Visible = false;
+            await Task.Delay(1000);
+
+            controlList[5].Visible = true;
+
+            clickCount++;
+            if (bubbleSort[clickCount] != 6)
+            {
+                MessageBox.Show("Загубихте!");
+                newGame();
+                return;
+            }
+
+            if ((clickCount + 1) == bubbleSort.Count)
+            {
+                startLevel();
+            }
+        }
+    }
+}
